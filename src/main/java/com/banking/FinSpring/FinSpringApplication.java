dc1ad44252a2d517ed484b1e156da309e3538049
Parth Sitoot <parth.sitoot@gmail.com>
2025-10-05 10:24:27 +0530

    Initial commit

---
+++ src/main/java/com/banking/FinSpring/FinSpringApplication.java
@@ -0,0 +1,13 @@
+package com.banking.FinSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/banking/FinSpring/FinSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banking.FinSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
