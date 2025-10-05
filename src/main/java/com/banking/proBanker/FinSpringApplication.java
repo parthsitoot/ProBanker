e3dd9132f9e3a38d3b8cd88aec3329702a0cf316
Parth Sitoot <parth.sitoot@gmail.com>
2025-10-05 10:28:57 +0530

    Initial commit

--- src/main/java/com/banking/proBanker/FinSpringApplication.java
+++ src/main/java/com/banking/proBanker/FinSpringApplication.java
@@ -1,4 +1,4 @@
-package com.banking.FinSpring;
+package com.banking.proBanker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/banking/proBanker/FinSpringApplicationTests.java
+++ src/test/java/com/banking/proBanker/FinSpringApplicationTests.java
@@ -1,4 +1,4 @@
-package com.banking.FinSpring;
+package com.banking.proBanker;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
