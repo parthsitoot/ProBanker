cfe2d539e8fd851b61d84a2f93063cc529ad5a46
Parth Sitoot <parth.sitoot@gmail.com>
2025-11-01 20:57:21 +0530

    Adding authentication

---
+++ src/main/java/com/banking/proBanker/Security/JwtAuthenticationFilter.java
@@ -0,0 +1,94 @@
+package com.banking.proBanker.Security;
+
+import com.banking.proBanker.Exceptions.InvalidTokenException;
+import com.banking.proBanker.Service.TokenService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.val;
+import org.hibernate.grammars.hql.HqlParser;
+import org.springframework.boot.micrometer.observation.autoconfigure.ObservationProperties;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+
+/**
+ * JWT Authentication Filter
+ * This filter intercepts incoming requests to authenticate user based on JWT tokens.
+ * It extends OncePerRequestFilter at ensure it's executed once per request.
+ */
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private final UserDetailsService userDetailsService;
+    private final TokenService tokenService;
+
+    /**
+     * Preforms the filtering for each request
+     * @param request       HTTP request
+     * @param response      HTTP request
+     * @param filterChain   filter chain
+     *
+     * @throws ServletException If a servlet-specific error occurs
+     * @throws IOExceptiion     If an I/O exception occurs
+     */
+
+    @Override
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain
+    ) throws ServletException, IOException {
+        if (SecurityContextHolder.getContext().getAuthentication() != null) {
+            logger.info ("User is already authenticated");
+
+            filterChain.doFilter(request, response);
+        }
+
+        val requestTokenHeader = request.getHeader("Authorization");
+
+        if (requestTokenHeader == null) {
+            filterChain.doFilter(request, response);
+        }
+
+        if (!requestTokenHeader.startsWith("Bearer ")) {
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED,
+                    "Token must start with 'Bearer '");
+
+            return;
+        }
+        val token = requestTokenHeader.substring(7);
+        String username = null;
+
+        try {
+            tokenService.validateToken(token);
+            username = tokenService.getUsernameFromToken(token);
+        } catch (InvalidTokenException e) {
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED,
+                    e.getMessage());
+
+            return;
+        }
+        val userDetails = userDetailsService.loadUserByUsername(username);
+
+        val authToken = new UsernamePasswordAuthenticationToken(
+                userDetails, null, userDetails.getAuthorities()
+        );
+
+        authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+        SecurityContextHolder.getContext().setAuthentication(authToken);
+        filterChain.doFilter(request, response);
+    }
+}
